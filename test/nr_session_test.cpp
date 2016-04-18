e832693f023c01dded7c520c417037f9c537f998
Luis Andres Marentes Cubillos <l_marent@yahoo.com>
2016-04-18 15:30:20 -0400

    Asynchronous messages

--- test/nr_session_test.cpp
+++ test/nr_session_test.cpp
@@ -30,8 +30,8 @@ class ResponderTest;
  */
 class nr_session_test : public nr_session {
   public:
-	nr_session_test(state_t state=STATE_ANSLP_CLOSE, uint32 msn=0)
-		: nr_session(state, msn) { }
+	nr_session_test(state_t state=STATE_ANSLP_CLOSE, anslp_config *conf=NULL, uint32 msn=0)
+		: nr_session(state, conf, msn) { }
 
 	friend class ResponderTest;
 };
@@ -42,6 +42,8 @@ class ResponderTest : public CppUnit::TestCase {
 	CPPUNIT_TEST_SUITE( ResponderTest );
 
 	CPPUNIT_TEST( testClose );
+	CPPUNIT_TEST( testPending );
+	CPPUNIT_TEST( testPendingInstalling );
 	CPPUNIT_TEST( testAuctioning );
 	CPPUNIT_TEST( testIntegratedStateMachine );
 
@@ -52,6 +54,8 @@ class ResponderTest : public CppUnit::TestCase {
 	void tearDown();
 	void add_fields(msg::anslp_ipap_message *mess);
 	void testClose();
+	void testPending();
+	void testPendingInstalling();
 	void testAuctioning();
 	void testIntegratedStateMachine();
 
@@ -230,44 +234,209 @@ ResponderTest::create_anslp_bidding(uint32 msn) const
 }
 
 void 
-ResponderTest::testClose() {
-	
-	
+ResponderTest::testClose() 
+{
+		
 	/*
-	 * CLOSE ---[rx_CREATE && CHECK_AA && CREATE(Lifetime>0) ]---> AUCTIONING
+	 * CLOSE ---[rx_CREATE && CHECK_AA && CREATE(Lifetime>0) ]---> PENDING
 	 */
-	nr_session_test s1(nr_session::STATE_ANSLP_CLOSE);
+	nr_session_test s1(nr_session::STATE_ANSLP_CLOSE, conf);
 
 	msg::ntlp_msg *msg = create_anslp_create();
 	event *e1 = new msg_event(NULL, msg, true);
 	 
 	process(s1, e1);
-	ASSERT_STATE(s1, nr_session::STATE_ANSLP_AUCTIONING);
-	ASSERT_RESPONSE_MESSAGE_SENT(d, information_code::sc_success);
+	ASSERT_STATE(s1, nr_session::STATE_ANSLP_PENDING);
 	ASSERT_TIMER_STARTED(d, s1.get_state_timer());
-	
+		
 	/*
-	 * CLOSE ---[rx_CREATE && CREATE(Lifetime > MAX) ]---> AUCTIONING
+	 * CLOSE ---[rx_CREATE && CREATE(Lifetime > MAX) ]---> PENDING
 	 */
-	nr_session_test s2(nr_session::STATE_ANSLP_CLOSE);
+	nr_session_test s2(nr_session::STATE_ANSLP_CLOSE, conf);
 	event *e2 = new msg_event(NULL,
 		create_anslp_create(START_MSN+1, 100000), true);
 	
 	process(s2, e2);
-	ASSERT_STATE(s2, nr_session::STATE_ANSLP_AUCTIONING);
-	ASSERT_RESPONSE_MESSAGE_SENT(d,
-		information_code::sc_success);
+	ASSERT_STATE(s2, nr_session::STATE_ANSLP_PENDING);
 	ASSERT_TIMER_STARTED(d, s2.get_state_timer());
 
 	/*
 	 * CLOSE ---[rx_BIDDING ]---> CLOSE
 	 */
-	nr_session_test s3(nr_session::STATE_ANSLP_CLOSE);
-	event *e3 = new msg_event(NULL,
-		create_anslp_bidding(START_MSN+1), true);
+	nr_session_test s3(nr_session::STATE_ANSLP_CLOSE, conf);
+	event *e3 = new msg_event(NULL, create_anslp_bidding(START_MSN+1), true);
 	
 	process(s3, e3);
 	ASSERT_STATE(s3, nr_session::STATE_ANSLP_CLOSE);
+	ASSERT_NO_MESSAGE(d);
+	ASSERT_NO_TIMER(d);
+		
+}
+
+
+void 
+ResponderTest::testPending() {
+	
+	
+	/*
+	 * STATE_ANSLP_PENDING ---[tg_check]---> STATE_ANSLP_PENDING_INSTALL
+	 */
+	nr_session_test s1(nr_session::STATE_ANSLP_CLOSE, conf);
+
+	msg::ntlp_msg *msg = create_anslp_create();
+	event *e1 = new msg_event(NULL, msg, true);
+	 
+	process(s1, e1);
+	ASSERT_STATE(s1, nr_session::STATE_ANSLP_PENDING);
+	ASSERT_TIMER_STARTED(d, s1.get_state_timer());
+	
+	vector<msg::anslp_mspec_object *> mspec_objects;
+	mspec_objects.push_back(mess1->copy());
+	mspec_objects.push_back(mess2->copy());
+	mspec_objects.push_back(mess3->copy());
+
+	event *e2 = new api_check_event(new session_id(s1.get_id()), mspec_objects, NULL);
+	
+	process(s1, e2);
+	ASSERT_STATE(s1, nr_session::STATE_ANSLP_PENDING_INSTALLING);
+	ASSERT_TIMER_STARTED(d, s1.get_state_timer());
+
+	/*
+	 * STATE_ANSLP_PENDING ---[STATE_TIMEOUT && counter > max_retry]---> STATE_ANSLP_CLOSE
+	 */
+	nr_session_test s5(nr_session::STATE_ANSLP_PENDING, conf);
+
+	s5.get_state_timer().set_id(47);
+	s5.set_max_retries(0); 
+	s5.set_last_create_message(create_anslp_create());
+	timer_event *e3 = new timer_event(NULL, 47);
+
+	process(s5, e3);
+	ASSERT_STATE(s5, nr_session::STATE_ANSLP_CLOSE);
+	ASSERT_RESPONSE_MESSAGE_SENT(d, information_code::sc_permanent_failure);
+	ASSERT_NO_TIMER(d);
+
+	/*
+	 * STATE_ANSLP_PENDING ---[STATE_TIMEOUT && counter <= max_retry]---> STATE_ANSLP_PENDING
+	 */
+	nr_session_test s6(nr_session::STATE_ANSLP_PENDING, conf);
+	
+	s6.get_state_timer().set_id(47);
+	s6.set_last_create_message(create_anslp_create());
+	s6.set_max_retries(3);	
+	timer_event *e4 = new timer_event(NULL, 47);
+
+	// We must to wait for a check successful response. 
+	process(s6, e4);
+	ASSERT_STATE(s6, nr_session::STATE_ANSLP_PENDING);
+	ASSERT_TIMER_STARTED(d, s6.get_state_timer());
+		
+	CPPUNIT_ASSERT(s6.get_create_counter()== 1);
+
+
+	/*
+	 * STATE_ANSLP_PENDING ---[rx_BIDDING ]---> STATE_ANSLP_PENDING
+	 */
+	nr_session_test s3(nr_session::STATE_ANSLP_PENDING, conf);
+	event *e7 = new msg_event(NULL, create_anslp_bidding(START_MSN+1), true);
+	
+	process(s3, e7);
+	ASSERT_STATE(s3, nr_session::STATE_ANSLP_PENDING);
+	ASSERT_NO_MESSAGE(d);
+	ASSERT_NO_TIMER(d);
+	
+}
+
+
+
+void 
+ResponderTest::testPendingInstalling() {
+	
+	
+	/*
+	 * STATE_ANSLP_PENDING_INSTALLING ---[tg_install(== Nbr objects)]---> AUCTIONING
+	 */
+	nr_session_test s1(nr_session::STATE_ANSLP_PENDING_INSTALLING, conf);
+	
+	s1.set_last_create_message(create_anslp_create());
+	s1.rule->set_request_object(mess1->copy());
+	s1.rule->set_request_object(mess2->copy());
+	s1.rule->set_request_object(mess3->copy());
+	
+	vector<msg::anslp_mspec_object *> mspec_objects;
+	mspec_objects.push_back(mess1->copy());
+	mspec_objects.push_back(mess2->copy());
+	mspec_objects.push_back(mess3->copy());
+
+	event *e2 = new api_install_event(new session_id(s1.get_id()), mspec_objects, NULL);
+	
+	process(s1, e2);
+	ASSERT_STATE(s1, nr_session::STATE_ANSLP_AUCTIONING);
+	ASSERT_RESPONSE_MESSAGE_SENT(d, information_code::sc_success);
+	ASSERT_TIMER_STARTED(d, s1.get_state_timer());
+	
+	
+	/*
+	 * STATE_ANSLP_PENDING_INSTALLING ---[tg_install(!= Nbr objects)]---> CLOSE
+	 */
+	nr_session_test s2(nr_session::STATE_ANSLP_PENDING_INSTALLING, conf);
+	s2.set_last_create_message(create_anslp_create());
+	s2.rule->set_request_object(mess1->copy());
+	s2.rule->set_request_object(mess2->copy());
+	s2.rule->set_request_object(mess3->copy());
+
+	vector<msg::anslp_mspec_object *> mspec_objects2;
+	mspec_objects2.push_back(mess1->copy());
+	mspec_objects2.push_back(mess2->copy());
+
+	event *e4 = new api_install_event(new session_id(s2.get_id()), mspec_objects2, NULL);
+	
+	process(s2, e4);
+	ASSERT_STATE(s2, nr_session::STATE_ANSLP_CLOSE);
+	ASSERT_RESPONSE_MESSAGE_SENT(d, information_code::sc_permanent_failure);
+	ASSERT_NO_TIMER(d);
+
+	/*
+	 * STATE_ANSLP_PENDING_INSTALLING ---[STATE_TIMEOUT && counter > max_retry]---> STATE_ANSLP_CLOSE
+	 */
+	nr_session_test s5(nr_session::STATE_ANSLP_PENDING_INSTALLING, conf);
+
+	s5.get_state_timer().set_id(47);
+	s5.set_max_retries(0); 
+	s5.set_last_create_message(create_anslp_create());
+	timer_event *e5 = new timer_event(NULL, 47);
+
+	process(s5, e5);
+	ASSERT_STATE(s5, nr_session::STATE_ANSLP_CLOSE);
+	ASSERT_RESPONSE_MESSAGE_SENT(d, information_code::sc_permanent_failure);
+	ASSERT_NO_TIMER(d);
+
+	/*
+	 * STATE_ANSLP_PENDING_INSTALLING ---[STATE_TIMEOUT && counter <= max_retry]---> STATE_ANSLP_PENDING
+	 */
+	nr_session_test s6(nr_session::STATE_ANSLP_PENDING_INSTALLING, conf);
+	
+	s6.get_state_timer().set_id(47);
+	s6.set_last_create_message(create_anslp_create());
+	s6.set_max_retries(3);	
+	timer_event *e6 = new timer_event(NULL, 47);
+
+	// We must to wait for a check successful response. 
+	process(s6, e6);
+	ASSERT_STATE(s6, nr_session::STATE_ANSLP_PENDING_INSTALLING);
+	ASSERT_TIMER_STARTED(d, s6.get_state_timer());
+		
+	CPPUNIT_ASSERT(s6.get_create_counter()== 1);
+
+
+	/*
+	 * STATE_ANSLP_PENDING_INSTALLING ---[rx_BIDDING ]---> STATE_ANSLP_PENDING_INSTALLING
+	 */
+	nr_session_test s3(nr_session::STATE_ANSLP_PENDING_INSTALLING, conf);
+	event *e7 = new msg_event(NULL, create_anslp_bidding(START_MSN+1), true);
+	
+	process(s3, e7);
+	ASSERT_STATE(s3, nr_session::STATE_ANSLP_PENDING_INSTALLING);
 	ASSERT_NO_MESSAGE(d);
 	ASSERT_NO_TIMER(d);
 	
@@ -281,7 +450,7 @@ ResponderTest::testAuctioning() {
 	/*
 	 * STATE_ANSLP_AUCTIONING ---[rx_REFRESH && CHECK_AA && REFRESH(Lifetime>0) ]---> STATE_ANSLP_AUCTIONING
 	 */
-	nr_session_test s1(nr_session::STATE_ANSLP_AUCTIONING, START_MSN);
+	nr_session_test s1(nr_session::STATE_ANSLP_AUCTIONING, conf, START_MSN);
 	event *e1 = new msg_event(NULL, create_anslp_refresh(START_MSN+1, 10), true);
 
 	process(s1, e1);
@@ -293,7 +462,7 @@ ResponderTest::testAuctioning() {
 	/*
 	 * STATE_ANSLP_AUCTIONING ---[rx_REFRESH && REFRESH(Lifetime > MAX) ]---> STATE_ANSLP_AUCTIONING
 	 */
-	nr_session_test s2(nr_session::STATE_ANSLP_AUCTIONING, START_MSN);
+	nr_session_test s2(nr_session::STATE_ANSLP_AUCTIONING, conf, START_MSN);
 	event *e2 = new msg_event(NULL,
 		create_anslp_refresh(START_MSN+1, 10), true);
 
@@ -306,7 +475,7 @@ ResponderTest::testAuctioning() {
 	/*
 	 * STATE_ANSLP_AUCTIONING ---[rx_REFRESH && REFRESH(Lifetime == 0) ]--->STATE_ANSLP_CLOSE
 	 */
-	nr_session_test s3(nr_session::STATE_ANSLP_AUCTIONING, START_MSN);
+	nr_session_test s3(nr_session::STATE_ANSLP_AUCTIONING, conf, START_MSN);
 	event *e3 = new msg_event(NULL,
 			create_anslp_refresh(START_MSN+1, 0), true);
 
@@ -318,7 +487,7 @@ ResponderTest::testAuctioning() {
 	/*
 	 * STATE_AUCTIONING_PART ---[rx_REFRESH && MSN too low ]---> STATE_AUCTIONING_PART
 	 */
-	nr_session_test s4(nr_session::STATE_ANSLP_AUCTIONING, START_MSN);
+	nr_session_test s4(nr_session::STATE_ANSLP_AUCTIONING, conf, START_MSN);
 	event *e4 = new msg_event(NULL, create_anslp_refresh(10, 20), true);
 	
 	process(s4, e4);
@@ -329,7 +498,7 @@ ResponderTest::testAuctioning() {
 	/*
 	 * STATE_AUCTIONING_PART ---[rx_BIDDING ]---> STATE_AUCTIONING_PART
 	 */
-	nr_session_test s5(nr_session::STATE_ANSLP_AUCTIONING, START_MSN);
+	nr_session_test s5(nr_session::STATE_ANSLP_AUCTIONING, conf, START_MSN);
 	event *e5 = new msg_event(NULL, create_anslp_bidding(START_MSN + 1), true);
 
 	process(s5, e5);
@@ -344,12 +513,39 @@ ResponderTest::testIntegratedStateMachine()
 {
 
 	/*
-	 * CLOSE ---[rx_CREATE && CHECK_AA && CREATE(Lifetime>0) ]---> AUCTIONING
+	 * CLOSE ---[rx_CREATE && CHECK_AA && CREATE(Lifetime>0) ]---> STATE_ANSLP_PENDING_INSTALLING.
 	 */
-	nr_session_test s1(nr_session::STATE_ANSLP_CLOSE);
+	nr_session_test s1(nr_session::STATE_ANSLP_CLOSE, conf);
 	event *e1 = new msg_event(NULL, create_anslp_create(), true);
 
 	process(s1, e1);
+	ASSERT_STATE(s1, nr_session::STATE_ANSLP_PENDING);
+	ASSERT_TIMER_STARTED(d, s1.get_state_timer());
+
+	vector<msg::anslp_mspec_object *> mspec_objects;
+	mspec_objects.push_back(mess1->copy());
+	mspec_objects.push_back(mess2->copy());
+	mspec_objects.push_back(mess3->copy());
+
+	event *e2 = new api_check_event(new session_id(s1.get_id()), mspec_objects, NULL);
+	
+	process(s1, e2);
+	ASSERT_STATE(s1, nr_session::STATE_ANSLP_PENDING_INSTALLING);
+	ASSERT_TIMER_STARTED(d, s1.get_state_timer());
+
+
+	/* 
+	 * STATE_ANSLP_PENDING_INSTALLING ---  [tg_install]  ---> AUCTIONING
+	 */
+	
+	// objects installed.
+	vector<msg::anslp_mspec_object *> mspec_objects2;
+	mspec_objects2.push_back(mess1->copy());
+	mspec_objects2.push_back(mess2->copy());
+	mspec_objects2.push_back(mess3->copy());
+
+	event *e3 = new api_install_event(new session_id(s1.get_id()), mspec_objects2, NULL);
+	process(s1, e3);
 	ASSERT_STATE(s1, nr_session::STATE_ANSLP_AUCTIONING);
 	ASSERT_RESPONSE_MESSAGE_SENT(d, information_code::sc_success);
 	ASSERT_TIMER_STARTED(d, s1.get_state_timer());
@@ -358,13 +554,12 @@ ResponderTest::testIntegratedStateMachine()
 	/*
 	 * STATE_ANSLP_AUCTIONING ---[rx_REFRESH && REFRESH(Lifetime == 0) ]--->STATE_ANSLP_CLOSE
 	 */
-	event *e2 = new msg_event(NULL,
+	event *e4 = new msg_event(NULL,
 			create_anslp_refresh(START_MSN+1, 0), true);
 
-	process(s1, e2);
+	process(s1, e4);
 	ASSERT_STATE(s1, nr_session::STATE_ANSLP_CLOSE);
 	ASSERT_RESPONSE_MESSAGE_SENT(d, information_code::sc_success);
 	ASSERT_NO_TIMER(d);
-
-
+	
 }
